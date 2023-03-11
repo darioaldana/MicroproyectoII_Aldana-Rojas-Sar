2cf0e724078291ad42d14e565dff30d3b9ae27bb
abrilsar <a.sar@correo.unimet.edu.ve>
2023-03-11 19:28:17 -0400

    api-deatilPage

--- src/api/api.js
+++ src/api/api.js
@@ -13,6 +13,7 @@ export async function fetchMovie(movie_id) {
 export async function fetchUpcomingMovies(page = 1) {
     return axios.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=${api_key}&language=en-US&page=${page}`)
 }
+export const base_url = 'https://image.tmdb.org/t/p/original';
 
 // export async function fetchMoviesByName(page = 1, name) {
 //     return axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&name=${name}&page=${page}&include_adult=false`)
