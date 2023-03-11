3c10ad28cb14194e35b7c2c2c51889bd44619122
DanielRojas-hub <69920982+DanielRojas-hub@users.noreply.github.com>
2023-03-11 19:40:07 -0400

    join parts of home page

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,9 @@ import ReactDOM from "react-dom/client";
 // import { LoginPage } from "./pages/LoginPage/LoginPage";
 import { HomePage } from "./pages/HomePage/HomePage";
 import "./index.css";
-import { HomeSection } from "./pages/HomeSection/HomeSection"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HomePage />
-    <HomeSection />
   </React.StrictMode>,
 )
