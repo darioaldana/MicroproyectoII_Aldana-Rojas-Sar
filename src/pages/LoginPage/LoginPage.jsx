c2b1d405f7319f30a7a45123c4e850aa5a826be5
Dario <dario.aldana@correo.unimet.edu.ve>
2023-03-11 13:34:47 -0400

    Pasar diseno a tailwind

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -45,21 +45,22 @@ export function LoginPage() {
     };
 
     return (
-      <div className={styles.wholePage}>
-      <div className={styles.container}>
-        <form className={styles.form} onSubmit={onSubmit}>
+      <div class="flex justify-center items-center w-full h-full">
+      <div class = "w-6/12 flex-wrap">
+        <form class ="bg-yellow-200 w-full p-12 grid mt-16 rounded-3xl grid-cols-1 gap-y-10" onSubmit={onSubmit}>
           {/* <img src={logo} className={styles.logo}/>  */}
-          <h1 className={styles.title}>Welcome!</h1>
-          <p className={styles.welcomeTxt}>
+          <h1 class = "text-4xl text-center font-semibold text-black bg-yellow-200 tracking-tighter">Welcome!</h1>
+          <p class = "font-normal text-base leading-6 text-center text-black mb-2 bg-yellow-200">
             Login we're glad to help you!
           </p>
 
           {/* EMAIL FIELD */}
           <div className={styles.inputContainer}>
-            <h3 htmlFor="email">
+            <h3 class = "block text-sm font-medium leading-5 mb-0 text-gray-800 bg-yellow-200" htmlFor="email">
               Email
             </h3>
             <input
+              class = "block w-full rounded-none text-base leading-6 font-normal bg-gray-400"
               type="email"
               name="email"
               id="email"
@@ -71,10 +72,11 @@ export function LoginPage() {
 
           {/* PASSWORD FIELD */}
           <div className={styles.inputContainer}>
-            <h3 htmlFor="password">
+            <h3 class = "block text-sm font-medium leading-5 mb-0 text-gray-800 bg-yellow-200"  htmlFor="password">
               Password
             </h3>
             <input
+              class = "block w-full rounded-none text-base leading-6 font-normal bg-gray-400"
               type="password"
               name="password"
               id="password"
@@ -83,26 +85,27 @@ export function LoginPage() {
               required
             />
           </div>
-          <div className={styles.ButtonCont}>
-            <button type="forgot" className={styles.forgotButton}>
+          <div class="grid-cols-2 inline-flex bg-yellow-200 gap-x-10 items-stretch justify-center">
+            <button type="forgot" class="block h-10 w-1/3  bg-gray-400 font-bold text-base leading-6 text-black cursor-pointer rounded-md">
               Forgot?
             </button>
-            <button type="submit" className={styles.submitButton}>
+
+            <button type="submit" class="block h-10 w-1/3 bg-gray-400 font-bold text-base leading-6 text-black cursor-pointer rounded-md">
               Login
             </button>
           </div>
 
           <button
             type="button"
-            className={styles.googleButton}
+            class = "block h-10 bg-gray-400 font-bold text-base leading-6 text-black cursor-pointer rounded-2xl"
             onClick={handleSignWithGoogle}
           >
             Continue with Google
           </button>
-          {/* TODO: inicio de sesion con facebook */}
+
           <button
             type="button"
-            className={styles.twitterButton}
+            class = "block h-10 bg-gray-400 font-bold text-base leading-6 text-black cursor-pointer rounded-2xl"
             onClick={handleSignWithGoogle}
           >
             Continue with Twitter
