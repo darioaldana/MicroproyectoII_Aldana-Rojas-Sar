6b61e6c958cecb3c41ed4fa4890743f3c3192591
abrilsar <a.sar@correo.unimet.edu.ve>
2023-03-12 13:17:31 -0400

    routes-publics

---
+++ src/constants/url.js
@@ -0,0 +1,4 @@
+export const LoginPageUrl ="/"
+export const RegisterPageUrl="/"
+export const DeatilPageUrl = "/deatil"
+export const HomePageUrl = "/home"
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { HomePage } from "./pages/HomePage/HomePage";
-import { DetailPage } from "./pages/DetailPage/DetailPage"
-
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { DetailPage } from "./pages/DetailPage/DetailPage";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import "./index.css";
+import {
+  DeatilPageUrl,
+  HomePageUrl,
+  LoginPageUrl,
+  RegisterPageUrl,
+} from "./constants/url";
+import { LoginPage } from "./pages/LoginPage/LoginPage";
+import { ProtectedRoutes } from "./ProtectedRoutes";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HomePage />
-    {/* <DetailPage /> */}
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path={LoginPageUrl} element={<LoginPage />} />
+        <Route path={RegisterPageUrl} element={<RegisterPage />} />
+        <Route path="*" element={<h1>Not Found!</h1>} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path={DeatilPageUrl} element={<DetailPage />} />
+          <Route path={HomePageUrl} element={<HomePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
