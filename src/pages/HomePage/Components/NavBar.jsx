afca1ff5e5b63246252c59b7e260876ae12bd2e9
abrilsar <a.sar@correo.unimet.edu.ve>
2023-03-12 10:20:57 -0400

    button-logout

--- src/pages/HomePage/Components/NavBar.jsx
+++ src/pages/HomePage/Components/NavBar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { fetchMoviesByName } from "../../../api/api";
+
 export function NavBar() {
   const [value, setValue] = useState("");
   const handleSearch = (e) => {
@@ -89,11 +89,32 @@ export function NavBar() {
           </form>
         </div>
         <div className="flex-col items-center">
-          <img
+          {/* <img
             className="w-10 h-10 rounded-full shadow-lg"
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png"
             alt="Bonnie image"
-          />
+          /> */}
+          <button
+            type="submit"
+            className="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-black bg-white rounded-lg border hover:bg-gray-100 focus:ring-4 focus:outline-none  lg:w-25"
+          >
+            <svg
+              aria-hidden="true"
+              className="w-5 h-5 -ml-1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                fillRule="evenodd"
+                d="M7.5 3.75A1.5 1.5 0 006 5.25v13.5a1.5 1.5 0 001.5 1.5h6a1.5 1.5 0 001.5-1.5V15a.75.75 0 011.5 0v3.75a3 3 0 01-3 3h-6a3 3 0 01-3-3V5.25a3 3 0 013-3h6a3 3 0 013 3V9A.75.75 0 0115 9V5.25a1.5 1.5 0 00-1.5-1.5h-6zm5.03 4.72a.75.75 0 010 1.06l-1.72 1.72h10.94a.75.75 0 010 1.5H10.81l1.72 1.72a.75.75 0 11-1.06 1.06l-3-3a.75.75 0 010-1.06l3-3a.75.75 0 011.06 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
         </div>
       </div>
     </div>
