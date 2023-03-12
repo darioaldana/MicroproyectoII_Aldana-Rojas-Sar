40c0328477387a1e16b0fe3617d624430a48a5aa
Dario <dario.aldana@correo.unimet.edu.ve>
2023-03-12 12:06:52 -0400

    Version final del login, i hope so

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -4,8 +4,12 @@ import React from 'react'
 
 // import { REGISTER_URL } from "../../constants/urls";
 import { useState } from "react";
+import GoogleLogo from "../../assets/google.png"
+import FacebookLogo from "../../assets/facebook.png"
+
 // import {loginWithEmailAndPassword, signInWithGoogle,} from "../../firebase/auth-service";
 import logo from '../../assets/logo.png'
+
 
 
 export function LoginPage() {
@@ -43,77 +47,89 @@ export function LoginPage() {
     };
 
     return (
-      <div class="flex justify-center items-center box-border bg-[#7e1616] w-screen h-screen p-0 m-0">
-
-        <form class ="bg-yellow-200 w-1/2 p-12 grid mt-16 rounded-3xl grid-cols-1 gap-y-5" onSubmit={onSubmit}>
-          {/* <img src={logo} className={styles.logo}/>  */}
-          <h1 class = "text-4xl text-center font-semibold text-black bg-yellow-200 tracking-tighter">Welcome!</h1>
-          <p class = "font-normal text-base leading-6 text-center text-black mb-2 bg-yellow-200">
+      <div className="isolate bg-white py-24 px-6 sm:py-32 lg:px-8">
+        <div class="mx-auto max-w-2xl text-center">
+          <h1 class = "text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Welcome!</h1>
+          <p class = "mt-2 text-lg leading-8 text-gray-600">
             Login to a new way of enjoying cinema
           </p>
+      </div>
 
-          {/* EMAIL FIELD */}
-          <div>
-            <h3 class = "block text-sm font-medium leading-5 mb-0 text-gray-800 bg-yellow-200" htmlFor="email">
-              Email
-            </h3>
-            <input
-              class = "block w-full rounded-none text-base leading-6 font-normal bg-gray-100 text-black placeholder:text-gray-400 pl-[14px] h-9"
-              type="email"
-              name="email"
-              id="email"
-              placeholder= "fulanito@mail.com" 
-              onChange={handleOnChange}
-              required
-            />
+      <form class ="mx-auto mt-16 max-w-xl sm:mt-20" onSubmit={onSubmit}>
+        
+        <div className="grid grid-cols-1 gap-y-6 gap-x-8 sm:grid-cols-2">
+
+          <div className="sm:col-span-2">
+            <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
+            Email
+            </label>
+            <div className="mt-2.5">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder='ej: juancito@trucupey.com'
+                onChange={handleOnChange}
+                className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                required
+              />
+            </div>
           </div>
 
-          {/* PASSWORD FIELD */}
-          <div>
-            <h3 class = "block text-sm font-medium leading-5 mb-0 text-gray-800 bg-yellow-200"  htmlFor="password">
+          <div className="sm:col-span-2">
+            <label htmlFor="company" className="block text-sm font-semibold leading-6 text-gray-900">
               Password
-            </h3>
-            <input
-              class = "block w-full rounded-none text-base leading-6 font-normal text-black bg-gray-100 placeholder:text-gray-400 pl-[14px] h-9"
-              type="password"
-              name="password"
-              id="password"
-              placeholder="********"
-              onChange={handleOnChange}
-              required
-            />
+            </label>
+            <div className="mt-2.5">
+              <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  onChange={handleOnChange}
+                  className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  required
+              />
+            </div>
           </div>
-          <div class="grid-cols-2 inline-flex bg-yellow-200 gap-x-10 items-stretch justify-center">
-            <button type="forgot" class="block h-10 w-1/3  bg-gray-400 font-bold text-base leading-6 text-black cursor-pointer rounded-md">
-              Forgot?
-            </button>
+        </div>
 
-            <button type="submit" class="block h-10 w-1/3 bg-gray-400 font-bold text-base leading-6 text-black cursor-pointer rounded-md">
-              Login
-            </button>
-          </div>
-
+        <div className="mt-10">
           <button
-            type="button"
-            class = "block h-10 font-bold text-base leading-6 text-black cursor-pointer rounded-2xl bg-red-400"
-            onClick={handleSignWithGoogle}
+            type="submit"
+            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Continue with Google
+            Login
+          </button>
+        </div>
+
+
+        <div className="flex items-center justify-center">
+          <button 
+            type = "button"
+            onClick = {handleSignWithGoogle}
+            className="flex flex-col items-center justify-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:scale-105">
+            <img src={GoogleLogo} className='h-12' />
+            <div className="items-center justify-center">
+              <h3 className="block font-semibold text-gray-900">Google</h3>
+            </div>
           </button>
 
-          <button
-            type="button"
-            class = "block h-10 font-bold text-base leading-6 text-black cursor-pointer rounded-2xl bg-blue-400"
-            onClick={handleSignWithGoogle}
-          >
-            Continue with Twitter
+          <button className="flex flex-col items-center justify-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:scale-105">
+            <img src={FacebookLogo} className='h-12 p-1' />
+            <div className="items-center justify-center">
+              
+              <h3 className="block font-semibold text-gray-900">Facebook</h3>
+            </div>
           </button>
 
-          {/* <Link to={REGISTER_URL} className={styles.loginRedirect}>
-            ¿No sign in yet?{" "}
-            <span className={styles.redirectLink}>SIGN IN</span>
-          </Link> */}
-        </form>
+
+        </div>
+
+      
+      </form>      
+        
+
+        
       </div>
     );
 }
